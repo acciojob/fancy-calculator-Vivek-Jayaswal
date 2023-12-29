4cd47a82b0c2a51cd68a29a4065dbe3edfec49e8
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2023-12-29 15:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const firstVar = prompt();
+const SecondVar = prompt();
+const ft = parseInt(firstVar)
+const st = parseInt(SecondVar);
+alert(`"The final result is: ${ft*2 + st}"`)
